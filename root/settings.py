cece0c686637ab9daacc593baa1a9f3c93602d80
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-08-07 12:42:05 +0000

    Database and SU setup

--- root/settings.py
+++ root/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +86,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- root/urls.py
+++ root/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from cms import views as index_views
+from cms.views import cms
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('cms', cms, name='cms'),
     path('admin/', admin.site.urls),
 ]

6a00d9b6027dc6461511517f620cb90c4f6d8601
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-08-14 11:35:31 +0000

    fixed views for string length

--- cms/views.py
+++ cms/views.py
@@ -41,6 +41,8 @@ def note_content(request, pk):
 
     :template:`cms/note_create.html`
     """
+
+
 @login_required
 def note_create(request, pk):
     # Fetch the client using the primary key (pk)
@@ -51,25 +53,31 @@ def note_create(request, pk):
         if form.is_valid():
             note = form.save(commit=False)
             note.client = client  # Associate the note with the fetched client
-            note.author = request.user  # Automatically set the author as the logged-in user (carer)
+            # Automatically set the author as the logged-in user (carer)
+            note.author = request.user
             try:
                 note.save()
                 logger.info(f"Saved note with slug: {note.slug}")
                 messages.add_message(
                     request, messages.SUCCESS,
                     f'Note created for {client.first_name} {client.last_name}.'
-                    )
-                return redirect('cms:note_content', pk=note.pk)  # Redirect to the newly created note's content page
+                )
+                # Redirect to the newly created note's content page
+                return redirect('cms:note_content', pk=note.pk)
             except Exception as e:
                 logger.error(f"Failed to save note: {e}")
-                messages.error(request, 'Failed to save note. Please try again.')
+                messages.error(
+                    request, 'Failed to save note. Please try again.')
         else:
             logger.warning("Form is not valid")
-            messages.warning(request, 'Form validation failed. Please correct the errors and try again.')
+            messages.warning(
+                request,
+                'Form validation failed. Please correct the errors and try again.')
     else:
         form = NoteForm()
 
-    return render(request, 'cms/note_create.html', {'client': client, 'form': form})
+    return render(request, 'cms/note_create.html',
+                  {'client': client, 'form': form})
 
 
 # Editing of notes
@@ -85,6 +93,8 @@ def note_create(request, pk):
 
     :template:`cms/note_edit.html`
     """
+
+
 @login_required
 def note_edit(request, pk):
     # Fetch the note object using the primary key (pk)
@@ -96,11 +106,14 @@ def note_edit(request, pk):
         if form.is_valid():
             form.save()
             messages.success(request, 'Note updated successfully.')
-            return redirect('cms:note_content', pk=note.pk)  # Redirect to the note content page after successful edit
+            # Redirect to the note content page after successful edit
+            return redirect('cms:note_content', pk=note.pk)
         else:
             # Log or handle form validation errors
             logger.warning("Form validation failed for note edit.")
-            messages.warning(request, 'Form validation failed. Please correct the errors and try again.')
+            messages.warning(
+                request,
+                'Form validation failed. Please correct the errors and try again.')
     else:
         # If GET request, instantiate the form with the existing note instance
         form = NoteForm(instance=note)
@@ -122,6 +135,8 @@ def note_edit(request, pk):
 
     Handled by JavaScript
     """
+
+
 @login_required
 def note_delete(request, pk):
     note = get_object_or_404(Note, pk=pk)
@@ -130,10 +145,13 @@ def note_delete(request, pk):
     # Ensure the user has the correct permissions
     if hasattr(request.user, 'client') and request.user.client.id == pk:
         # The user is the client themselves, deny edit access, only view
-        return render(request, 'cms/error.html', {'error_message': 'Clients cannot delete their own notes.'})
+        return render(request, 'cms/error.html',
+                      {'error_message': 'Clients cannot delete their own notes.'})
     elif hasattr(request.user, 'carer') and request.user.carer.clients.filter(pk=pk).exists():
         # The user is not a carer for this client, deny editing
-        messages.error(request, 'Carers cannot delete notes of clients they do not manage. Please contact an administrator if this note needs to be removed.')
+        messages.error(
+            request,
+            'Carers cannot delete notes of clients they do not manage. Please contact an administrator if this note needs to be removed.')
         return redirect('cms:client_detail', pk=client.pk)
 
     if request.method == 'POST':
@@ -163,6 +181,8 @@ def note_delete(request, pk):
 
     :template:`cms/client_list.html`
     """
+
+
 @login_required
 def client_list(request):
     if request.method == "GET":
@@ -170,7 +190,8 @@ def client_list(request):
         clients = Client.objects.all()
 
         if query:
-            # Filter clients based on the query and check if the user is the client or a carer
+            # Filter clients based on the query and check if the user is the
+            # client or a carer
             clients = clients.filter(first_name__icontains=query)
 
         # Further filter the clients based on the user's relationship
@@ -185,7 +206,8 @@ def client_list(request):
                 # If the user is not a client or carer, restrict access
                 clients = Client.objects.none()
                 # Update for error page
-                return render(request, 'cms/error.html')  # Update for error page
+                # Update for error page
+                return render(request, 'cms/error.html')
 
         return render(request, 'cms/client_list.html', {'clients': clients})
 
@@ -203,6 +225,8 @@ def client_list(request):
 
     :template:`cms/add_client.html`
     """
+
+
 @login_required
 def client_new(request):
     if request.method == "POST":
@@ -228,6 +252,8 @@ def client_new(request):
 
     :template:`cms/client_form.html`
     """
+
+
 @login_required
 def client_edit(request, pk):
     client = get_object_or_404(Client, pk=pk)
@@ -235,7 +261,8 @@ def client_edit(request, pk):
     # Ensure the user has the correct permissions
     if hasattr(request.user, 'client') and request.user.client.id == pk:
         # The user is the client themselves, deny edit access, only view
-        return render(request, 'cms/error.html', {'error_message': 'You do not have permission to edit this client.'})
+        return render(request, 'cms/error.html',
+                      {'error_message': 'You do not have permission to edit this client.'})
     elif hasattr(request.user, 'carer') and request.user.carer.clients.filter(pk=pk).exists():
         # The user is a carer for this client, allow editing
         if request.method == "POST":
@@ -243,16 +270,17 @@ def client_edit(request, pk):
             if form.is_valid():
                 form.save()
                 messages.add_message(
-                    request, messages.SUCCESS,
-                    f'{client.first_name} {client.last_name} has been successfully updated.'
-                )
+                    request, messages.SUCCESS, f'{
+                        client.first_name} {
+                        client.last_name} has been successfully updated.')
                 return redirect('cms:client_list')
         else:
             form = ClientForm(instance=client)
         return render(request, 'cms/client_form.html', {'form': form})
     else:
         # The user is neither the client nor a carer associated with the client
-        return render(request, 'cms/error.html', {'error_message': 'You do not have permission to edit this client.'})
+        return render(request, 'cms/error.html',
+                      {'error_message': 'You do not have permission to edit this client.'})
 
 
 # Delete client
@@ -268,12 +296,17 @@ def client_edit(request, pk):
 
     :template:`**`
     """
+
+
 @login_required
 def client_delete(request, pk):
     client = get_object_or_404(Client, pk=pk)
 
     # Ensure the user has the correct permissions
-    if hasattr(request.user, 'carer') and request.user.carer.clients.filter(pk=pk).exists():
+    if hasattr(
+            request.user,
+            'carer') and request.user.carer.clients.filter(
+            pk=pk).exists():
         if request.method == 'POST':
             # The user confirmed deletion, so delete the client
             client_name = f"{client.first_name} {client.last_name}"
@@ -286,10 +319,13 @@ def client_delete(request, pk):
             return redirect('cms:client_list')
         else:
             # Render a confirmation page before deletion
-            return render(request, 'cms/client_confirm_delete.html', {'client': client})
+            return render(request,
+                          'cms/client_confirm_delete.html',
+                          {'client': client})
     else:
         # The user is not a carer associated with the client, deny deletion
-        return render(request, 'cms/error.html', {'error_message': 'You do not have permission to delete this client.'})
+        return render(request, 'cms/error.html',
+                      {'error_message': 'You do not have permission to delete this client.'})
 
 
 # Detail view for a client
@@ -306,12 +342,17 @@ def client_detail(request, pk):
         elif hasattr(request.user, 'carer') and request.user.carer.clients.filter(pk=pk).exists():
             pass  # The user is a carer for this client, so they are allowed to view the details
         else:
-            # The user is neither the specific client nor a carer associated with the client
-            return render(request, 'cms/error.html', {'error_message': 'You do not have permission to view this client.'})
+            # The user is neither the specific client nor a carer associated
+            # with the client
+            return render(request, 'cms/error.html',
+                          {'error_message': 'You do not have permission to view this client.'})
 
     # Determine if the user is allowed to edit/delete notes
     carer_for_client = False
-    if hasattr(request.user, 'carer') and request.user.carer.clients.filter(pk=pk).exists():
+    if hasattr(
+            request.user,
+            'carer') and request.user.carer.clients.filter(
+            pk=pk).exists():
         carer_for_client = True
 
     context = {
